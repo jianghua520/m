d1984d645a0a6a1db568fec934ebcb2b464b5030
江华 <62876649+jianghua520@users.noreply.github.com>
2020-08-07 01:11:18 +0800

    1

---
+++ admin/lele.config.php
@@ -0,0 +1,20 @@
+<?php
+ $lele = array (
+  'danmuon' => 'on',
+  'color' => '#83639D',
+  'logo' => '/img/logo.png',
+  'waittime' => '3',
+  'sendtime' => '3',
+  'dmrule' => 'http://baidu.com',
+  'pbgjz' => '操ABCDEFGHIJKLMNOPQRSTUVWSYZabcdefghijklmnopqrstuvwsyz',
+  'ads' => 
+  array (
+    'pause' => 
+    array (
+      'state' => 'off',
+      'pic' => 'http://v.datll.com/upload/dtplayer/20200309-1/c2d2432bb47782498c345f8917d36d56.jpg',
+      'link' => 'http://baidu.com',
+    ),
+  ),
+);
+?>
