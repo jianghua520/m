f1a6b93f9b94c701a4bfdb1c8b83d4dc163b49e9
江华 <62876649+jianghua520@users.noreply.github.com>
2020-08-07 01:14:42 +0800

    1

---
+++ admin/data.php
@@ -0,0 +1,27 @@
+<?php
+ $lele = array (
+  'danmuon' => 'on',
+  'color' => '#775e80',
+  'logo' => 'https://cdn.jsdelivr.net/gh/lelege/bilibili/img/logo.png',
+  'waittime' => '3',
+  'sendtime' => '3',
+  'dmliyi' => '弹幕礼仪',
+  'dmrule' => 'https://www.5mrk.com',
+  'yjtest' => '关于BTJSON',
+  'yjrule' => 'https://wpa.qq.com/msgrd?v=3&uin=8852422&site=qq&menu=yes',
+  'pbgjz' => '操ABCDEFGHIJKLMNOPQRSTUVWSYZabcdefghijklmnopqrstuvwsyz',
+  'ads' => 
+  array (
+    'pause' => 
+    array (
+      'state' => 'on',
+      'pic' => 'https://i.loli.net/2019/06/06/5cf8c5d9cec8510758.jpg',
+      'link' => 'http://baidu.com',
+    ),
+  ),
+  'autoplay' => 'true',
+  'bilibili' => '',
+  'pmdzd' => '9%',
+  'pmdzdy' => '<marquee direction="down"><FONT face=楷体_GB2312 color=#999 size=300><STRONG>我是一个蹦蹦跳跳的跑马灯，是不是超级可爱，我还会各种出场方式喔~</FONT></STRONG></marquee>',
+);
+?>
